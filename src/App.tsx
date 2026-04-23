06eb4749fdb620bcd4820f9e122e2c0eae25cde7
Melis <tyn.melis.ru@gmail.com>
2026-04-23 15:21:42 +0600

    Добавил логику генерации чисел

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+import { useState } from 'react';
+
 const App = () => {
-  return <div>App</div>;
+  const [numbers, setNumbers] = useState([5, 7, 10, 23, 31]);
+  const getRandomNum = () => {
+    const randomNumbers: number[] = [];
+    while (true) {
+      const randomInt: number = Math.floor(Math.random() * 32 + 5);
+      const unicCheck = randomNumbers.includes(randomInt);
+      if (!unicCheck) {
+        randomNumbers.push(randomInt);
+      }
+      if (randomNumbers.length >= 5) {
+        break;
+      }
+    }
+    randomNumbers.sort((a, b) => a - b);
+    setNumbers([...randomNumbers]);
+  };
+  return (
+    <div>
+      <button className="btn" type="button" onClick={getRandomNum}>
+        Получить числа
+      </button>
+      {numbers.map((num, i) => (
+        <li key={i}>{num}</li>
+      ))}
+    </div>
+  );
 };
 
 export default App;
